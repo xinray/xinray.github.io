099720d774fd175afa962bcda11eacef95b30d9e
ray <277450201@qq.com>
2016-10-02 01:27:57 +0800

    add function

--- javascripts/base.js
+++ javascripts/base.js
@@ -4,4 +4,16 @@ $('document').ready(function(){
 
         $('.navbar').slideToggle('fast');
     });
+    var backToTop = document.querySelector('.back-to-top ');
+    var backToTopA = document.querySelector('.back-to-top a');
+    window.addEventListener('scroll',function () {
+
+        var scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop)
+
+        if (scrollTop > 200) {
+            backToTop.classList.add('back-to-top-show')
+        } else {
+            backToTop.classList.remove('back-to-top-show')
+        }
+    })
 });
